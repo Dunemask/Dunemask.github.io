f56157c63fae2d5553ffce3f03aef01b0265e121
Elijah Parker <Dunemask@users.noreply.github.com>
2018-03-28 21:44:38 -0600

    Created Prototype

--- java/projects/DunemaskSetup/src/setup/Test.java
+++ java/projects/DunemaskSetup/src/setup/Test.java
@@ -1,10 +1,14 @@
 package setup;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import dunemask.dm.CMD;
+import dunemask.dunemasking.GitHub;
 import dunemask.dunemasking.Setup;
 import dunemask.dunemasking.StreamGobbler;
 import dunemask.util.FileUtil;
@@ -15,8 +19,19 @@ public class Test {
 	public static final String jarName="Run.jar";
 	public static void main(String[] args) {
 		String top = dunemask.dunemasking.Setup.init(Setup.autoHandleSetup, jarName, "cookie");
-		File[] subs = FileUtil.getAllSubFiles(new File(top+"export\\"));
+		int c = JOptionPane.showConfirmDialog(null, "The Installation Of Dunemasking Will now Begin, Admin Rights/Interweb Required, Proceed?");
+		switch (c) {
+		case 1:System.exit(0);
+			break;
+		case 2: System.exit(0);
+			break;
 		
+		}
+		File l = GitHub.gitFile("dunemask.github.io", "java/dunemask_libraries/Dunemasking3.86.jar");
+		
+		
+		File[] subs = FileUtil.getAllSubFiles(new File(top+"export\\"));
+	
 		
 		System.out.println("top:"+top);
 		String programFilesDir = System.getenv("ProgramFiles")+"\\";
@@ -32,12 +47,36 @@ public class Test {
 				cmds.add(CMD.copyFileViaCmd(f, new File(dunemaskingPath+f.getAbsolutePath().replace(top+"export\\", ""))));
 			}
 		}
-		//cmds.add("pause");
-		//System.out.println("com:"+cmds.get(1));
+		cmds.add(CMD.copyFileViaCmd(l,new File(dunemaskingPath+l.getName())));
+		try {
+			File helloWorld = File.createTempFile("HelloWorld", ".java");
+			RW.write(helloWorld,new String[] 
+					{"public class HelloWorld {",
+					 "	public static void main(String[] args){",
+					 "  System.out.println(\"HelloWorld!\");}}"
+					},0);
+			cmds.add(CMD.copyFileViaCmd(helloWorld, new File(dunemaskingPath+"HelloWorld.java")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		copyAndClean(args, top);
+		JOptionPane.showMessageDialog(null, "Thanks for Installing Dunemasking!");
+	
+	
+	}
+	/**
+	 * 
+	 */
+	private static void copyAndClean(String[] cmds,String top) {
 		CMD.openElevatedCmd(cmds);
-		
+		if(JarUtil.isJar(jarName)) {
+			System.out.println("Cookie!");
+			FileUtil.deleteAllSubFolders(new File(top));
+			System.out.println("top "+top);
+		}
 		
 	}
+	
 
 
 }
